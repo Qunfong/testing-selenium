94fce6bd9ac2d45e9dc9d3a781d027acb2510e2d
Qun <qun.wang@capgemini.com>
2020-02-07 08:57:57 +0100

    Learning how to work with POM pattern and testing dropdown elements. https://testautomationu.applitools.com/selenium-webdriver-tutorial-java/chapter4.3.html

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,29 +5,33 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
+        goHome();
+    }
+
+    @BeforeMethod
+    public void goHome(){
         driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
+    }
 
-        driver.manage().window().setSize(new Dimension(375, 812));
-
-        //Returns first found element in DOM
-        //WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        //inputsLink.click();
-
-        //Returns link elements found
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
-
-        System.out.println(driver.getTitle());
-        driver.quit(); //closes the session vs close()
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
 
     public static void main(String args[]){
